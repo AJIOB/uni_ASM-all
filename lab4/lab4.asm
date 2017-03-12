83c2b0f780e80416a76a566f4015f2243f1460f4
AJIOB <aleksandr9809@gmail.com>
2017-03-12 13:44:20 +0300

    lab4: Первая часть поля готова (минающее слово Snake). Макрос begin убран и явно перенесен в текст

--- lab4/lab4.asm
+++ lab4/lab4.asm
@@ -2,17 +2,16 @@
 
 
 ;macro HELP
-begin MACRO
-	mov ax, @data	;init
-	mov ds, ax
-	mov es, ax
-
-	xor ax, ax
-ENDM
-
 endd MACRO
 	mov ah, 4ch
 	int 21h
+ENDM
+
+clearScreen MACRO
+	push ax
+	mov ax, 0003h
+	int 10h
+	pop ax
 ENDM
 
 ;end macro help
@@ -30,21 +29,56 @@ appleBody equ 0Fh		;symbol SUN
 xField equ 50
 yField equ 21
 
-screen dw 0020h, 0010h, 0031h, 0032h, 0033h
+videoStart equ 0B800h
+space equ 0020h
+fieldSpacingBad equ space, 0FBAh, xField dup(space)
+fieldSpacing equ fieldSpacingBad, 0FBAh
+grSym equ 0AFDCh
+grSpc equ 0AF20h
+
+screen	dw xSize dup(space)
+		dw space, 0FC9h, xField dup(0FCDh), 0FCBh, xSize - xField - 5 dup(0FCDh), 0FBBh, space
+firstBl	dw fieldSpacing, xSize - xField - 5 dup(grSpc), 0FBAh, space
+		dw fieldSpacing, grSpc, 4 dup(grSym), 15 dup(grSpc), 4 dup(grSym), grSpc, 0FBAh, space
+		dw fieldSpacing, grSpc, grSym, 5 dup(grSpc), 3 dup(grSym), 2 dup(grSpc), 3 dup(grSym), grSpc, grSym, 3 dup(grSpc), grSym, 2 dup(grSpc), grSym, grSpc, 0FBAh, space
+		dw fieldSpacing, grSpc, 4 dup(grSym), grSpc, grSym, 2 dup(grSpc), grSym, grSpc, grSym, 2 dup(grSpc), 3 dup(grSym, grSpc), 4 dup(grSym), grSpc, 0FBAh, space
+		dw fieldSpacing, 4 dup(grSpc), grSym, grSpc, grSym, 2 dup(grSpc), grSym, grSpc, 4 dup(grSym), grSpc, 2 dup(grSym), 2 dup(grSpc), grSym, 4 dup(grSpc), 0FBAh, space
+		dw fieldSpacing, grSpc, 4 dup(grSym), grSpc, grSym, 2 dup(grSpc), grSym, grSpc, grSym, 2 dup(grSpc), 3 dup(grSym, grSpc), 4 dup(grSym), grSpc, 0FBAh, space
+		dw fieldSpacing, xSize - xField - 5 dup(grSpc), 0FBAh, space
+delim1	dw fieldSpacingBad, 0FCCh, xSize - xField - 5 dup(0FCDh), 0FB9h, space
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
+		dw xSize dup(space)
 .code
 
 main:
-	begin
+	mov ax, @data	;init
+	mov ds, ax
+	mov ax, videoStart
+	mov es, ax
+	xor ax, ax
 
-;set video and clear monitor
-	mov ax, 0003h
-	int 10h
+	clearScreen
 
 	call initField
 
 	call mainGame
 
 to_close:
+	;clearScreen
 
 	endd
 
@@ -52,14 +86,17 @@ to_close:
 
 initField PROC
 	mov si, offset screen
-	mov di, 0B800h
+	xor di, di
 
-	mov cx, 5
+	mov cx, xSize*ySize
 	rep movsw
+
+	ret
 ENDP
 
 mainGame PROC
 	;todo
+	ret
 ENDP
 
 ;end procedure help
