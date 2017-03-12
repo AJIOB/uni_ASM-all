8333bab80931a03c8c71cd86b575e014747ec913
AJIOB <aleksandr9809@gmail.com>
2017-03-12 22:30:48 +0300

    lab4: Добавлен выход по Esc. Добавлены константы границ поля. Теперь в змейке в начале хранится голова, выделено доп. место в массиве для переполнения при переписывании. Добавлено перенаправление на выход (т.к. было слишком много строк кода). Добавлен код сдвига змейки (пока не тестировался).

--- lab4/lab4.asm
+++ lab4/lab4.asm
@@ -28,6 +28,7 @@ KMoveUp equ 11h		;W key
 KMoveDown equ 1Fh	;S key
 KMoveLeft equ 1Eh	;A key
 KMoveRight equ 20h	;D key
+KExit equ 01h 		;ESC key
 
 xSize equ 80
 ySize equ 25
@@ -35,12 +36,17 @@ xField equ 50
 yField equ 21
 oneMemoBlock equ 2
 
-videoStart equ 0B800h
+videoStart dw 0B800h
+dataStart dw 0000h
 space equ 0020h
 snakeBodySymbol equ 0A40h
 appleSymbol equ 0B0Fh
-fieldSpacingBad equ space, 0FBAh, xField dup(space)
-fieldSpacing equ fieldSpacingBad, 0FBAh
+VWallSymbol equ 0FBAh
+HWallSymbol equ 0FCDh
+
+
+fieldSpacingBad equ space, VWallSymbol, xField dup(space)
+fieldSpacing equ fieldSpacingBad, VWallSymbol
 rbSym equ 0CFDCh	;white block with red background
 rbSpc equ 0CF20h	;space with red background
 ylSym equ 06FDCh	;white block with yellow background
@@ -49,31 +55,31 @@ grSym equ 02FDBh	;white block with green background
 grSpc equ 02F20h	;space with green background
 
 screen	dw xSize dup(space)
-		dw space, 0FC9h, xField dup(0FCDh), 0FCBh, xSize - xField - 5 dup(0FCDh), 0FBBh, space
-firstBl	dw fieldSpacing, xSize - xField - 5 dup(rbSpc), 0FBAh, space
-		dw fieldSpacing, rbSpc, 4 dup(rbSym), 15 dup(rbSpc), 4 dup(rbSym), rbSpc, 0FBAh, space
-		dw fieldSpacing, rbSpc, rbSym, 5 dup(rbSpc), 3 dup(rbSym), 2 dup(rbSpc), 3 dup(rbSym), rbSpc, rbSym, 3 dup(rbSpc), rbSym, 2 dup(rbSpc), rbSym, rbSpc, 0FBAh, space
-		dw fieldSpacing, rbSpc, 4 dup(rbSym), rbSpc, rbSym, 2 dup(rbSpc), rbSym, rbSpc, rbSym, 2 dup(rbSpc), 3 dup(rbSym, rbSpc), 4 dup(rbSym), rbSpc, 0FBAh, space
-		dw fieldSpacing, 4 dup(rbSpc), rbSym, rbSpc, rbSym, 2 dup(rbSpc), rbSym, rbSpc, 4 dup(rbSym), rbSpc, 2 dup(rbSym), 2 dup(rbSpc), rbSym, 4 dup(rbSpc), 0FBAh, space
-		dw fieldSpacing, rbSpc, 4 dup(rbSym), rbSpc, rbSym, 2 dup(rbSpc), rbSym, rbSpc, rbSym, 2 dup(rbSpc), 3 dup(rbSym, rbSpc), 4 dup(rbSym), rbSpc, 0FBAh, space
-		dw fieldSpacing, xSize - xField - 5 dup(rbSpc), 0FBAh, space
-delim1	dw fieldSpacingBad, 0FCCh, xSize - xField - 5 dup(0FCDh), 0FB9h, space
-secondF	dw fieldSpacing, xSize - xField - 5 dup(ylSpc), 0FBAh, space
+		dw space, 0FC9h, xField dup(HWallSymbol), 0FCBh, xSize - xField - 5 dup(HWallSymbol), 0FBBh, space
+firstBl	dw fieldSpacing, xSize - xField - 5 dup(rbSpc), VWallSymbol, space
+		dw fieldSpacing, rbSpc, 4 dup(rbSym), 15 dup(rbSpc), 4 dup(rbSym), rbSpc, VWallSymbol, space
+		dw fieldSpacing, rbSpc, rbSym, 5 dup(rbSpc), 3 dup(rbSym), 2 dup(rbSpc), 3 dup(rbSym), rbSpc, rbSym, 3 dup(rbSpc), rbSym, 2 dup(rbSpc), rbSym, rbSpc, VWallSymbol, space
+		dw fieldSpacing, rbSpc, 4 dup(rbSym), rbSpc, rbSym, 2 dup(rbSpc), rbSym, rbSpc, rbSym, 2 dup(rbSpc), 3 dup(rbSym, rbSpc), 4 dup(rbSym), rbSpc, VWallSymbol, space
+		dw fieldSpacing, 4 dup(rbSpc), rbSym, rbSpc, rbSym, 2 dup(rbSpc), rbSym, rbSpc, 4 dup(rbSym), rbSpc, 2 dup(rbSym), 2 dup(rbSpc), rbSym, 4 dup(rbSpc), VWallSymbol, space
+		dw fieldSpacing, rbSpc, 4 dup(rbSym), rbSpc, rbSym, 2 dup(rbSpc), rbSym, rbSpc, rbSym, 2 dup(rbSpc), 3 dup(rbSym, rbSpc), 4 dup(rbSym), rbSpc, VWallSymbol, space
+		dw fieldSpacing, xSize - xField - 5 dup(rbSpc), VWallSymbol, space
+delim1	dw fieldSpacingBad, 0FCCh, xSize - xField - 5 dup(HWallSymbol), 0FB9h, space
+secondF	dw fieldSpacing, xSize - xField - 5 dup(ylSpc), VWallSymbol, space
 		dw fieldSpacing, ylSpc, 06F53h, 06F63h, 06F6Fh, 06F72h, 06F65h, 06F3Ah, ylSpc
-	score	dw 4 dup(06F30h), 13 dup(ylSpc), 0FBAh, space
-		dw fieldSpacing, xSize - xField - 5 dup(ylSpc), 0FBAh, space
+	score	dw 4 dup(06F30h), 13 dup(ylSpc), VWallSymbol, space
+		dw fieldSpacing, xSize - xField - 5 dup(ylSpc), VWallSymbol, space
 		dw fieldSpacing, ylSpc, 06F53h, 06F70h, 2 dup(06F65h), 06F64h, 06F3Ah, ylSpc
-	speed	dw 06F30h, 16 dup(ylSpc), 0FBAh, space
-		dw fieldSpacing, xSize - xField - 5 dup(ylSpc), 0FBAh, space
-delim2	dw fieldSpacingBad, 0FCCh, xSize - xField - 5 dup(0FCDh), 0FB9h, space
-thirdF	dw fieldSpacing, xSize - xField - 5 dup(grSpc), 0FBAh, space
-		dw fieldSpacing, grSpc, 02F4Dh, 02F61h, 02F64h, 02F65h, grSpc, 02F62h, 02F79h, 02F3Ah, 10 dup(grSpc), 02FDCh, 3 dup(grSym), 02FDCh, grSpc, 0FBAh, space
-		dw fieldSpacing, 19 dup(grSpc), grSym, 02FDDh, grSym, 02FDEh, grSym, grSpc, 0FBAh, space
-		dw fieldSpacing, 2 dup(grSpc), 02FDCh, 02FDFh, grSym, 2 dup(grSpc), 2 dup(grSym, grSpc), 02FDEh, 2 dup(grSym), grSpc, grSym, 02FDFh, 02FDDh, grSpc, 5 dup(grSym), grSpc, 0FBAh, space
-		dw fieldSpacing, 2 dup(grSpc), grSym, 02FDCh, grSym, 2 dup(grSpc), 4 dup(grSym, grSpc), grSym, 02FDFh, 02FDCh, grSpc, grSym, grSpc, 02FDFh, grSpc, grSym, grSpc, 0FBAh, space
-		dw fieldSpacing, grSpc, 2 dup(02FDCh, 2 dup(grSym, grSpc)), 2 dup(grSym), 02FDDh, grSpc, grSym, 02FDCh, grSym, grSpc, 02FDFh, grSym, 02FDCh, grSym, 02FDFh, grSpc, 0FBAh, space
-		dw fieldSpacing, xSize - xField - 5 dup(grSpc), 0FBAh, space
-		dw space, 0FC8h, xField dup(0FCDh), 0FCAh, xSize - xField - 5 dup(0FCDh), 0FBCh, space
+	speed	dw 06F30h, 16 dup(ylSpc), VWallSymbol, space
+		dw fieldSpacing, xSize - xField - 5 dup(ylSpc), VWallSymbol, space
+delim2	dw fieldSpacingBad, 0FCCh, xSize - xField - 5 dup(HWallSymbol), 0FB9h, space
+thirdF	dw fieldSpacing, xSize - xField - 5 dup(grSpc), VWallSymbol, space
+		dw fieldSpacing, grSpc, 02F4Dh, 02F61h, 02F64h, 02F65h, grSpc, 02F62h, 02F79h, 02F3Ah, 10 dup(grSpc), 02FDCh, 3 dup(grSym), 02FDCh, grSpc, VWallSymbol, space
+		dw fieldSpacing, 19 dup(grSpc), grSym, 02FDDh, grSym, 02FDEh, grSym, grSpc, VWallSymbol, space
+		dw fieldSpacing, 2 dup(grSpc), 02FDCh, 02FDFh, grSym, 2 dup(grSpc), 2 dup(grSym, grSpc), 02FDEh, 2 dup(grSym), grSpc, grSym, 02FDFh, 02FDDh, grSpc, 5 dup(grSym), grSpc, VWallSymbol, space
+		dw fieldSpacing, 2 dup(grSpc), grSym, 02FDCh, grSym, 2 dup(grSpc), 4 dup(grSym, grSpc), grSym, 02FDFh, 02FDCh, grSpc, grSym, grSpc, 02FDFh, grSpc, grSym, grSpc, VWallSymbol, space
+		dw fieldSpacing, grSpc, 2 dup(02FDCh, 2 dup(grSym, grSpc)), 2 dup(grSym), 02FDDh, grSpc, grSym, 02FDCh, grSym, grSpc, 02FDFh, grSym, 02FDCh, grSym, 02FDFh, grSpc, VWallSymbol, space
+		dw fieldSpacing, xSize - xField - 5 dup(grSpc), VWallSymbol, space
+		dw space, 0FC8h, xField dup(HWallSymbol), 0FCAh, xSize - xField - 5 dup(HWallSymbol), 0FBCh, space
 		dw xSize dup(space)
 
 snakeMaxSize equ 20
@@ -81,8 +87,8 @@ snakeSize db 2
 PointSize equ 2
 
 ; XYh coordinates
-; first position - tail
-snakeBody dw 1B0Ch, 1C0Ch, snakeMaxSize-2 dup(0000h)
+; first position - head
+snakeBody dw 1C0Ch, 1B0Ch, snakeMaxSize-1 dup(0000h)
 
 stopVal equ 00h
 forwardVal equ 01h
@@ -98,6 +104,7 @@ waitTime db 10h
 main:
 	mov ax, @data	;init
 	mov ds, ax
+	mov dataStart, ax
 	mov ax, videoStart
 	mov es, ax
 	xor ax, ax
@@ -135,6 +142,13 @@ CalcOffsetByPoint MACRO
 	pop dx ax
 ENDM
 
+;ZF = 1 - buffer is free
+;AH = scan-code
+CheckBuffer MACRO
+	mov ax, 11h
+	int 16h
+ENDM
+
 ;end macro help
 
 ;procedure help
@@ -151,7 +165,7 @@ ENDP
 
 mainGame PROC
 ;load base snake
-	push ax bx cx ds es
+	push ax bx cx dx ds es
 
 	xor ch, ch
 	mov cl, snakeSize
@@ -170,8 +184,90 @@ loopInitSnake:
 	stosw
 	loop loopInitSnake
 
+
+
+checkAndMoveLoop:
+	
+	CheckBuffer
+	jz noSymbolInBuff
+
+	;exit
+	cmp ah, KExit
+	je endLoop_relink
+
+noSymbolInBuff:
+
+;MoveSnake:
+	;сдвигаем все тело
+		
+	mov al, snakeSize
+	xor ah, ah 		;в ah - длина массива
+	mov cx, ax 		;cx - счетчик кол-ва сдвигов
+	mov di, offset snakeBody
+	add di, ax 		;di - адрес следующего после последнего элемента массива
+	mov si, di
+	dec si 			;si - адрес последнего элемента массива
+
+	;удалить конец змейки с экрана
+	mov bx, ds:[si]
+	CalcOffsetByPoint
+	mov di, bx			;установили память, куда будем писать пробел
+	mov ax, space
+	stosw
+
+	mov es, dataStart	;для работы с данными
+	std				;идем от конца к началу
+	rep movsw
+
+	mov bx, snakeBody 	;текущая позиция головы
+
+	add bh, Bmoveright
+	add bl, Bmovedown	;новая позиция головы
+	mov snakeBody, bx	;сохраняем новую позицию головы
+	;все тело в памяти сдвинуто
+
+	CalcOffsetByPoint
+	mov ax, es:[bx]		;в ax текущий символ в памяти es:bx (куда должна стать змейка)
+
+	cmp ax, appleSymbol
+	je AppleIsNext
+
+	cmp ax, snakeBodySymbol
+	je SnakeIsNext
+
+	cmp ax, HWallSymbol
+	je PortalUpDown
+
+	cmp ax, VWallSymbol
+	je PortalLeftRight
+
+	jmp GoNext
+
+endLoop_relink:
+	jmp endLoop
+	
+AppleIsNext:
 	;todo
-	pop es ds cx bx ax
+SnakeIsNext:
+	;todo
+PortalUpDown:
+	;todo
+PortalLeftRight:
+	;todo
+GoNext:
+	
+	mov es, videoStart
+	mov di, bx		;вывести новое начало змейки
+	mov ax, snakeBodySymbol
+	stosw
+
+	;mov di, offset snakeBody + snakeSize
+
+	jmp checkAndMoveLoop
+endLoop:
+
+	;todo
+	pop es ds dx cx bx ax
 	ret
 ENDP
 
